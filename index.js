f63bdc6c6c71da199f2f5144212389be7a83e025
cyber-eternal <92edgarsargsyan@gmail.com>
2019-10-08 16:50:51 +0400

    changes in lib

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-var success=(a=200,b,c)=>buildResponse(a,b,c),failure=(a=409,b,c)=>buildResponse(a,b,c),buildResponse=(a,b,c={})=>{const d={statusCode:a,headers:{"Access-Control-Allow-Origin":"*","Access-Control-Allow-Credentials":!0,"Content-Type":"application/json",...c},body:b?JSON.stringify(b):""};return d};module.exports={success,failure};
+var success = (status = 200, body, headers) => {
+  return buildResponse(status, body, headers);
+};
+
+var failure = (status = 409, body, headers) => {
+  return buildResponse(status, body, headers);
+};
+
+var buildResponse = (statusCode, body, headers = {}) => {
+  const response = {
+    status: statusCode,
+    statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'application/json',
+      ...headers
+    },
+    body: body ? JSON.stringify(body) : '',
+  }
+  return response;
+};
+
+// var custom = ({ statusCode, response, contentType, location }) => {
+//   console.log('**** custom callback active ****');
+//   console.log(statusCode);
+//   console.log(response);
+//   return buildResponse(statusCode, response, contentType, location);
+// };
+
+// varimageBuffer = (statusCode, imageBuffer) => {
+//   return {
+//     statusCode,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Credentials': true,
+//       'Content-Type': 'image/png',
+//     },
+//     body: imageBuffer,
+//     isBase64Encoded: true
+//   };
+// };
+
+module.exports = {
+  success,
+  failure
+}
